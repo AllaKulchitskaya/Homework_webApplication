14bfacbd0e5ddb204e5c601188d6c843c4dc7c6e
AllaKulchitskaya <alla070@list.ru>
2022-12-28 22:01:05 +0300

    Первый коммит

---
+++ src/main/java/me/alla/homework_therecipewebsite/HomeworkTheRecipeWebsiteApplication.java
@@ -0,0 +1,13 @@
+package me.alla.homework_therecipewebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkTheRecipeWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkTheRecipeWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/alla/homework_therecipewebsite/HomeworkTheRecipeWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package me.alla.homework_therecipewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkTheRecipeWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
